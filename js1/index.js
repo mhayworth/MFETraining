14ca2e297182bbd8761bef086b0ed6441afb5004
mhayworth <73895101+mhayworth@users.noreply.github.com>
2024-12-02 10:36:01 -0500

    initial commit

---
+++ js1/index.js
@@ -0,0 +1,12 @@
+console.log("hello from index.js");
+
+//const = final
+const balance = 100;
+const account = "Michael";
+
+//separate with comma, automatic unpacks
+console.log(balance, account);
+
+let mutableBalance = 100;
+mutableBalance = 200;
+console.log(mutableBalance);
